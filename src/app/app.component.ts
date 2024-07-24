f55846b3a428131eb40a0cc21a73b147c89975cd
Pablo <pablogarcirodriguez@gmail.com>
2024-07-24 12:33:09 +0200

    Create user-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css',
+  styles: ``,
 })
 export class AppComponent {
+  city = 'Barcelona';
   title = 'angular-17-app';
 }
